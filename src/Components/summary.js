ea2c1f88270eafa3669d65c73daa989e3390ecda
jaganath10486 <jaganathsegi104@gmail.com>
2022-11-06 19:24:47 +0530

    3/4 done

--- src/Components/summary.js
+++ src/Components/summary.js
@@ -1,51 +1,59 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import Box from './box'
 import styles from '../Styles/summary.module.css';
-// import {AppContext} from './../Context/ContextApi.js'
+import AddTask from './AddTask';
 
-function Summary(props) 
-{
-//   const {budget, items} = useContext(AppContext);
-//   let spent = items.reduce((total, item) => {return (total + item.itemCost)}, 0);
-//   console.log(budget, spent);
-  const budget = 0;
-  const spent = 0;
+import { AppContext } from '../Context/ContextApi';
+
+
+function Summary(props) {
+  const {ongoing, Total, delayed, Completed} = useContext(AppContext);
   const hrstyle = {
-    width : '100%'
+    width: '100%'
   };
-  
+
   return (
-    <div className={styles.summary}>
+    <div className={styles.whole}>
+      <div className={styles.summary}>
         <div className={styles.summary_top}>
-            <div className={styles.summary_tasks}>
-                Tasks
-            </div>
-            <div className={styles.date}>
-               <i class="fa fa-chevron-left" aria-hidden="true"></i>
-               <i class="fa fa-calendar" aria-hidden="true"></i>
-               <span className={styles.currdate}>Nov 6, 2022</span>
-               <i class="fa fa-chevron-right" aria-hidden="true"></i>
-            </div>
+          <div className={styles.summary_tasks}>
+            Tasks
+          </div>
+          <div className={styles.date}>
+            <i class="fa fa-chevron-left" aria-hidden="true"></i>
+            <i class="fa fa-calendar" aria-hidden="true"></i>
+            <span className={styles.currdate}>Nov 6, 2022</span>
+            <i class="fa fa-chevron-right" aria-hidden="true"></i>
+          </div>
         </div>
-        <hr style={hrstyle}/>
-        <div className= {`row mt-3 ${styles.summary_down}`}>
-					<div className='col-sm'>
-						<Box className = 'warning' number = {budget} text ={'Ongoing'}>
-                        </Box>
-					</div>
-					<div className='col-sm'>
-						<Box className = 'danger' number = {budget} text={'Delayed'}>
-                       </Box>
-					</div>
-					<div className='col-sm'>
-						<Box className = 'success' number = {spent} text={'Completed'}>
-                        </Box>
-					</div>
-                    <div className='col-sm'>
-						<Box className = 'info' number = {spent} text={'Total Assigned'}>
-                        </Box>
-					</div>
-				</div>
+        <hr style={hrstyle} />
+        <div className={`row mt-3 ${styles.summary_down}`}>
+          <div className='col-sm'>
+            <Box className='warning' number={ongoing} text={'Ongoing'}>
+            </Box>
+          </div>
+          <div className='col-sm'>
+            <Box className='danger' number={delayed} text={'Delayed'}>
+            </Box>
+          </div>
+          <div className='col-sm'>
+            <Box className='success' number={Completed} text={'Completed'}>
+            </Box>
+          </div>
+          <div className='col-sm'>
+            <Box className='info' number={Total} text={'Total Assigned'}>
+            </Box>
+          </div>
+        </div>
+      </div>
+      <div className={styles.add}>
+        <div className={styles.addTask}>
+          <AddTask/>
+        </div>
+        <div className={styles.sort}>
+
+        </div>
+      </div>
     </div>
   )
 }
